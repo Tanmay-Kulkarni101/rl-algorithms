ab8445390fe4b9c5e7a452b47c804cc9376549db
Tanmay Kulkarni <tanmaygk97@gmail.com>
2021-12-16 15:21:51 -0500

    basic setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from os.path import join, dirname, realpath
+from setuptools import setup
+import sys
+
+assert sys.version_info.major == 3 and sys.version_info.minor >= 7, \
+    "This repo is designed to work with Python 3.6 and greater." \
+    + "Please install it before proceeding."
+
