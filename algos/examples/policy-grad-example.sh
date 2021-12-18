70760cda359dff41f7b83de81cf03d70f2397c3d
Tanmay Kulkarni <tanmaygk97@gmail.com>
2021-12-18 17:48:15 -0500

    add shell script to run example

---
+++ algos/examples/policy-grad-example.sh
@@ -0,0 +1,12 @@
+python ../src/vanilla-policy-gradient/vanilla-policy-gradient.py \
+--env CartPole-v0 \
+--hidden_sizes 64 64 \
+--gamma 0.99
+--seed 0
+--steps 4000
+--epochs 50
+--pi_lr 3e-4
+--v_lr 1e-3
+--value_func_iters 80
+--lamb 0.97
+--max_episode_length 1000
